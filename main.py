fe96c3dfa8276fbaa25b84b611ea85c682a2ea0c
Styro457 <raduursache6@gmail.com>
2024-12-28 20:44:36 +0200

    Add main_menu function

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ import pygame
 import constants
 from audio_manager import Audio_Manager
 from Object_Classes.player_controller import Player
+from Object_Classes.ui import Button
 import Object_Classes.tile_class as tiles
 
 def start():
     Game("Example game", constants.WIDTH, constants.HEIGHT, Audio_Manager(), None, fullscreen=False)
 
 def main_menu():
+    game = constants.game
+    game.objects.clear()
+    play_button = Button("game_sprites/ladder.png", "game_sprites/spike.png", "game_sprites/generic.png",
+                         (constants.CENTER_WIDTH, constants.CENTER_HEIGHT), (100, 100), level1)
+
+def level1():
     game = constants.game
     game.objects.clear()
     player = Player('game_sprites/temp_player_sprite.png', pygame.Vector2(300, 400), constants.ObjectType.PLAYER)
