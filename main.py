736e80d64d0b051c5ba91183eddda7d8bc00c263
Styro457 <raduursache6@gmail.com>
2024-12-28 20:35:51 +0200

    Fix old files

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
-from game import Game
-from object import Object
+from game_class import Game
+from Object_Classes.base_object import BaseObject
+import pygame
 import constants
 from audio_manager import Audio_Manager
 from Object_Classes.player_controller import Player
 import Object_Classes.tile_class as tiles
-from levels import level_1
 
-import levels
 def start():
-    Game("Example game", constants.WIDTH, constants.HEIGHT, None, None, fullscreen=False)
+    Game("Example game", constants.WIDTH, constants.HEIGHT, Audio_Manager(), None, fullscreen=False)
 
 def main_menu():
-    constants.game.objects.clear()
-    
-    sprite = "game_sprites/player.jpg"
-    object1 = Object("object", sprite, position = (constants.WIDTH/2, constants.HEIGHT/2), ObjectType = 0)
     game = constants.game
     game.objects.clear()
     player = Player('game_sprites/temp_player_sprite.png', pygame.Vector2(300, 400), constants.ObjectType.PLAYER)
     game.camera.target = player
-    tiles.draw_tile_list(level_1.level_1_screen_1)
+    tiles.draw_tile_list(constants.tile_set_example)
 
 start()
 main_menu()
