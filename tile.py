d9d25c2debad28bda2606233bf44337d0d4d6c1c
Sultan-Alshehry <sultan.shell123@gmail.com>
2024-12-28 03:09:38 +0200

    added tile init

--- tile.py
+++ tile.py
@@ -1,4 +1,11 @@
 import pygame
 
 class Tile():
-    def __init__(self):
+    def __init__(self, position, is_real, is_lethal, is_climbable, sprite, sound_effect):
+        super().__init__()
+        self.position = position
+        self.is_real = is_real
+        self.is_climbable = is_climbable
+        self.sprite = sprite
+        self.sound_effect = sound_effect
+
