7f526a6a03178d782f701e0fab00829c0d7cf3ec
Caleb <orangasus@gmail.com>
2024-12-29 20:57:54 +0200

    menu updates

--- main.py
+++ main.py
@@ -1,41 +1,17 @@
-from game_class import Game
-import pygame
 import constants
 from Managers.audio_manager import AudioManager
 from Managers.visual_manager import VisualManager
-from Object_Classes.player_controller import Player
-from Object_Classes.ui import Button, Label
-from Visual_Effects.animation import SpriteSheet
-from Visual_Effects.effects import CircleScreenTransition
-import Object_Classes.tile_class as tiles
-from levels import level_1
+from game_class import Game
+from scene_functions import main_menu
+
 
 def start():
-    Game("Example game", constants.WIDTH, constants.HEIGHT, AudioManager(), VisualManager("Assets/images"), fullscreen=False)
+    Game("Example game", constants.WIDTH, constants.HEIGHT, AudioManager(), VisualManager("Assets/images"),
+         fullscreen=False)
     constants.set_tile_textures()
 
-def main_menu():
-    game = constants.game
-    game.objects.clear()
-    title_label = Label(None, (game.width//2, game.height//2 + 150), (200, 50))
-    play_button = Button(game.get_sprite("ladder"), game.get_sprite("spike"), game.get_sprite("generic"),
-                         (constants.CENTER_WIDTH, constants.CENTER_HEIGHT), (100, 100), start_button)
-
-def start_button():
-    ve = CircleScreenTransition(60, (255, 255, 255), 50, 70, 8, 1, level1)
-    ve.start()
-
-def level1():
-    game = constants.game
-    game.objects.clear()
-    player = Player(game.get_sprite("spike"), pygame.Vector2(300, 400))
-    game.player = player
-    #animation = SpriteSheet(game.get_sprite("Owlet_Monster_Idle_4"), 32, 32, 100)
-    #player.play_animation(animation)
-    #game.camera.target = player
-    tiles.draw_tile_list(level_1.level_1_screen_1)
 
 start()
 main_menu()
-#level1()
+# level1()
 constants.game.loop()
---
+++ scene_functions.py
@@ -0,0 +1,31 @@
+import Object_Classes.tile_class as tiles
+import constants
+from Object_Classes.player_controller import Player
+from Object_Classes.ui import Button, Label
+from Visual_Effects.effects import CircleScreenTransition
+from levels import level_1
+import pygame
+
+
+def main_menu():
+    game = constants.game
+    game.objects.clear()
+    #title_label = Label(None, (game.width // 2, game.height // 2 + 150), (200, 50))
+    play_button = Button(game.get_sprite("ladder"), game.get_sprite("spike"), game.get_sprite("generic"),
+                         (constants.CENTER_WIDTH, constants.CENTER_HEIGHT), (100, 100), start_button)
+
+
+def start_button():
+    ve = CircleScreenTransition(60, (255, 255, 255), 50, 70, 8, 1, level1)
+    ve.start()
+
+
+def level1():
+    game = constants.game
+    game.objects.clear()
+    player = Player(game.get_sprite("spike"), pygame.Vector2(300, 400))
+    game.player = player
+    # animation = SpriteSheet(game.get_sprite("Owlet_Monster_Idle_4"), 32, 32, 100)
+    # player.play_animation(animation)
+    # game.camera.target = player
+    tiles.draw_tile_list(level_1.level_1_screen_1)
