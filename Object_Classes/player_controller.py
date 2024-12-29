11bde6c62e81b4348281f59bfd9864cfab925549
Styro457 <raduursache6@gmail.com>
2024-12-29 16:28:35 +0200

    I hate this

--- Object_Classes/player_controller.py
+++ Object_Classes/player_controller.py
@@ -207,10 +207,10 @@ class Player(BaseObject):
         self.currently_collides['bottom'] = self.get_collision_bottom()
 
     def get_collision_right(self):
-        return self.get_collision_base(self.top_horizontal_velocity, 0)
+        return self.get_collision_base(self.immediate_x_vel+1, 0)
 
     def get_collision_top(self):
-        return self.get_collision_base(0, -self.top_vertical_velocity)
+        return self.get_collision_base(0, self.immediate_x_vel-1)
 
     def get_collision_bottom(self):
         return self.get_collision_base(0, self.immediate_y_vel + 1)
