4ffc4d03df9a36d4aa214d2e1fc50a6eb177c95e
Styro457 <raduursache6@gmail.com>
2024-12-30 02:32:44 +0200

    Add colission to a tile

--- constants.py
+++ constants.py
@@ -29,7 +29,7 @@ class ObjectType(Enum):
     FAKE_PLAYER = 44
 
 collidable = [ObjectType.GENERIC, ObjectType.LADDER, ObjectType.SPIKE, ObjectType.NEXT_SCREEN_TRANSITION, ObjectType.PREVIOUS_SCREEN_TRANSITION,
-              100, 101, 102, 172, 173, 174, 175, 107, 108, 177, 178, 106, 124, 126, 127, 176, 224, 196, 197, 198, 199, 201, 179, 148, 148, 150, 200, 223, 220, 221, 222, 225]
+              100, 101, 102, 172, 173, 174, 175, 107, 108, 177, 178, 106, 124, 126, 127, 176, 224, 196, 197, 198, 199, 201, 179, 148, 148, 150, 200, 223, 220, 221, 222, 225, 155]
 climable = [ObjectType.LADDER]
 deadly = [ObjectType.SPIKE]
 
